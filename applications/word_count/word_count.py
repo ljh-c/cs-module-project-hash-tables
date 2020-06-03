4702417a1f2071c0f97a6cf398a44683c322d627
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-06-03 11:44:15 -0700

    Add solution to word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,20 @@
 def word_count(s):
-    # Your code here
+    table = str.maketrans(dict.fromkeys('":;,.-+=/\\|[]{}()*^&'))
 
+    strings = s.translate(table).split()
 
+    count = {}
+
+    for w in [word.lower() for word in strings]:
+        if w not in count:
+            count[w] = 0
+        
+        count[w] += 1
+    
+    return count
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
-    print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count('Hello^, my* cat. And:& [my] +cat doesn\'t say "hello" back. - hey'))
+    print(word_count('This (is) a /test of | {the} =emergency broadcast network; This is only a test, peeps.'))
