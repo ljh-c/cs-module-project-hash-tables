9aaa8db60a8508196130ba1c4ed2ac6292ef7e20
ljh-c <57099181+ljh-c@users.noreply.github.com>
2020-06-03 17:09:18 -0700

    Add solution to markov

--- applications/markov/markov.py
+++ applications/markov/markov.py
@@ -5,9 +5,42 @@ with open("input.txt") as f:
     words = f.read()
 
 # TODO: analyze which words can follow other words
-# Your code here
+markov = {}
 
+arr = words.split()
+
+markov[arr[0]] = []
+
+curr = arr[0]
+
+for word in arr[1:]:
+    if curr not in markov:
+        markov[curr] = []
+
+    markov[curr].append(word)
+
+    curr = word
+
+# print(random.choice(list(markov.keys())))
 
 # TODO: construct 5 random sentences
-# Your code here
+for _ in range(5):
+    start = " "
 
+    while not (start[0].isupper() or (start[0] == "\"" and start[:2].isupper())):
+        start = random.choice(list(markov.keys()))
+
+    print(start, end=" ", flush=True)
+
+    next_word = random.choice(markov[start])
+
+    print(next_word, end=" ", flush=True)
+
+    while not (next_word[-1] == "." or next_word[-1] == "?" or next_word[-1] == "!" or next_word[-2:] == ".\"" or next_word[-2:] == "?\"" or next_word[-2:] == "!\""):
+        next_word = random.choice(markov[next_word])
+        
+        print(next_word, end=" ", flush=True)
+    
+    print("\n")
+
+input("Press enter to close program.")
